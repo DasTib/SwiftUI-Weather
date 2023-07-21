8e31feac7851057a342f4d9769ef130b37770ce5
Joseph Ollis <35369747+DasTib@users.noreply.github.com>
2023-07-21 18:23:50 -0500

    Added weather image and temperature

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -13,19 +13,37 @@ struct ContentView: View {
             LinearGradient(gradient: Gradient(colors: [.blue, .white]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            .edgesIgnoringSafeArea(.all)
             VStack {
                 Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill" )
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
                     
+                    
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                    
+                    
+                }
+                Spacer()
             }
-            
         }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
             
+        }
     }
-}
+
